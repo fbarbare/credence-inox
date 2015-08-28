92e27ffe976e4b82809a4b37fdee642b5608e163
Florian <florian.barbare@gmail.com>
2015-08-28 18:33:20 +0100

    changes

--- index.php
+++ index.php
@@ -7,26 +7,29 @@
 ?>
 
 <div class="container-fluid content-container">
-  <div class="container">
-    <div class="page-header">
-      <h2>Credence en inox <small>Sur mesure ou standard</small></h2>
-    </div>
-    <div class="row">
-      <div class="col-md-6">
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
+  <div class="container content">
+    <div class="content-table">
+    <div class="content-row">
+      <div class="content-cell content-description">
+        <h2>Credence en inox <small>Sur mesure ou standard</small></h2>
+        <div class="container-fluid">
+          <div class="content-text">
+              <h3>Un inox GARANTIE a vie</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+          </div><div class="content-text">
+              <h3>Un inox magnetique inoxidable</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-6">
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
-        <h1>Hello, world!</h1>
+      <div class="content-cell content-image-container">
+        <img class="content-image" src="/img/index/credence.jpg"/>
       </div>
     </div>
   </div>
