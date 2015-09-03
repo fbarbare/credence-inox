744fa1c5b7d452f9afa0df9754dfb664db157390
Florian Barbare <florian.barbare@gmail.com>
2015-09-03 08:31:32 +0100

    start add basket + account to menu and display categories in index.php

--- index.php
+++ index.php
@@ -8,6 +8,37 @@
 
 <div class="content">
 
+  <div class="container-fluid section-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div style="width: 100%;">
+            <div style="margin-top: 100%;"></div>
+          </div>
+          <div><div>Crédences en inox</div><div>Voir plus de produits</div></div>
+        </div>
+        <div class="col-md-3">
+          <div style="width: 100%;">
+            <div style="margin-top: 100%;"></div>
+          </div>
+          <div><div>Crédences en inox</div><div>Voir plus de produits</div></div>
+        </div>
+        <div class="col-md-3">
+          <div style="width: 100%;">
+            <div style="margin-top: 100%;"></div>
+          </div>
+          <div><div>Crédences en inox</div><div>Voir plus de produits</div></div>
+        </div>
+        <div class="col-md-3">
+          <div style="width: 100%;">
+            <div style="margin-top: 100%;"></div>
+          </div>
+          <div><div>Crédences en inox</div><div>Voir plus de produits</div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="container-fluid section-container warranty">
     <div class="container">
       <div class="row">
